33e32507aff6cd947fdda86e8fb4ca66ef116a99
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-10 20:41:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ client.on("message", async message => {
    message.channel.send(hEmbed)
  }
   
+  if(command === "invite") {
+  message.delete().catch();	 
+   let hEmbed = new Discord.RichEmbed()
+   .setTitle("Ninja Gen Invite")
+   .setColor("0xff80ff")
+   .addField("You Can Invite Me Here", "[Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=544049582959755264&permissions=2146958839&scope=bot)")
+   message.channel.send(hEmbed)
+ }
+  
   if(command === "minecraft") {
     message.delete().catch();
     let Accounts = ["COMING SOON"];
