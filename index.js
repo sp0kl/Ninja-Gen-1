d0e60d1de5f87ac4115ee67f0521157b3d786caa
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-13 20:55:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -59,10 +59,11 @@ client.on("message", async message => {
  }
   
    if(command === "categories") {
-  message.delete().catch();	 
+  message.delete().catch();
+   const ID = client.users.get("name", `${message.author.Name}`).id;
    let hEmbed = new Discord.RichEmbed()
    .setTitle("Account Categories")
-   .setDescription("${message.author.mention} Here you can see a list of accounts i offer and how many are currently in stock")
+   .setDescription(`${ID} Here you can see a list of accounts i offer and how many are currently in stock`)
    .setColor("53380")
    .addField("Minecraft", "**0** Accounts available")
    .addField("Spotify", "**19** Accounts available")
