7d3a7ebd3e9c7d4125856a17465681dfab3f2a76
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-21 20:26:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ client.on("message", async message => {
    .setColor("0x0ffff")
    .addField("You Can Invite Me Here", "[Ninja Gen Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=544049582959755264&permissions=2146958839&scope=bot)")
     .setFooter(`Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
-   .setThumbnail(`https://i.imgur.com/xerUkNI.png`)
+   .setThumbnail(`https://i.imgur.com/xerUkNI.png`);
    message.channel.send(hEmbed)
  }
   
