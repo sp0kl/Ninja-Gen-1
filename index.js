271ca2f1aaecc3afe1d53d01e176de5fed2e2634
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-24 21:31:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -199,6 +199,71 @@ talkedRecently.add(message.author.id);
     })
   }
 
+   if(command === "spotify1") {
+ 
+    let Uembed = new Discord.RichEmbed()
+      .setColor("0xff0000")
+      .setTitle("Minecraft Accounts Status")
+      .setDescription(`<@${message.author.id}>` + " Sorry this category is currently unavailable :frowning: Join our [Support Server](https://discord.gg/CRhNHBs) for updates when this category becomes available");
+      message.delete().catch();
+   
+   
+   
+    if(message.author.id !== config.ownerID) return message.channel.send(Uembed)
+        snekfetch.get(`http://ip-api.com/json/${args}`).then (r => {
+          message.delete().catch();
+
+   let Cembed = new Discord.RichEmbed() 
+      .setTitle("Hmm :thinking: To Fast!!")
+      .setDescription(`<@${message.author.id}>` + " Please wait ``1 Minute`` before retyping this command. :shrug:")
+      .setColor("0xff0000")
+    message.delete().catch();
+
+    if (talkedRecently.has(message.author.id)) {
+            message.channel.send(Cembed);
+    } else {
+   
+      let Accounts = ["testing123:testing123"];
+   
+      let result = Math.floor((Math.random() * Accounts.length));
+   
+      let Gembed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("Your Minecraft account")
+      .setDescription(Accounts[result]);
+   
+   
+      let Membed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("Minecraft Account generated")
+      .setDescription(`<@${message.author.id}>` + " Your Minecraft account has been sent to your dms, Please check them if the account is a duplicate or doesn't work use the command again until it does")
+     
+      message.channel.send(Membed)
+      message.author.send(Gembed)
+
+   let dEmbed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("User has generated a account")
+      .setDescription(`<@${message.author.id}>` + ` has generated a Minecraft account the account info is ${Accounts[result]}`);
+   
+
+      client.fetchUser('444609097233465347').then((user) => {
+        user.send(dEmbed)
+        });
+
+        // Adds the user to the set so that they can't talk for a minute
+talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 60000);
+    }
+   
+   
+    })
+  }
+
+  
   
   if(command === "spotify") {
     message.delete().catch();
@@ -380,7 +445,7 @@ talkedRecently.add(message.author.id);
 
 
 
-  })
+  });
 
   
    if(command === "nordvpn") {
