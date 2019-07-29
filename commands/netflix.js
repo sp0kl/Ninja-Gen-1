396b6e518e2bfc3b7606be47a1aa5f1d7f85bf2a
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-28 20:23:01 -0600

    Update netflix.js

--- commands/netflix.js
+++ commands/netflix.js
@@ -12,42 +12,62 @@ let Uembed = new Discord.RichEmbed()
       message.delete().catch();
    
    
+   let  owners =  client.fetchUser('444609097233465347')
    
-    if(message.author.id !== owners) return message.channel.send(Uembed)
+    if(message.author.id !== 444609097233465347) return message.channel.send(Uembed)
         snekfetch.get(`http://ip-api.com/json/${args}`).then (r => {
           message.delete().catch();
+
+   let Cembed = new Discord.RichEmbed() 
+      .setTitle("Hmm :thinking: To Fast!!")
+      .setDescription(`<@${message.author.id}>` + " Please wait ``1 Minute`` before retyping this command. :shrug:")
+      .setColor("0xff0000")
+    message.delete().catch();
+
+    if (talkedRecently.has(message.author.id)) {
+            message.channel.send(Cembed);
+    } else {
+   
+      let Accounts = ["testing123:testing123"];
+   
+      let result = Math.floor((Math.random() * Accounts.length));
+   
+      let Gembed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("Your Minecraft account")
+      .setDescription(Accounts[result]);
+   
+   
+      let Membed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("Minecraft Account generated")
+      .setDescription(`<@${message.author.id}>` + " Your Netflix account has been sent to your dms, Please check them if the account is a duplicate or doesn't work use the command again until it does")
      
-    let Accounts = ["COMING SOON"];
+      message.channel.send(Membed)
+      message.author.send(Gembed)
 
-    let result = Math.floor((Math.random() * Accounts.length));
+   let dEmbed = new Discord.RichEmbed()
+      .setColor(53380)
+      .setTitle("User has generated a account")
+      .setDescription(`<@${message.author.id}>` + ` has generated a Netflix account the account info is ${Accounts[result]}`);
+   
 
-    let dEmbed = new Discord.RichEmbed()
-    .setColor(53380)
-    .setTitle("User has generated a account")
-    .setDescription(`<@${message.author.id}>` + ` has generated a Netflix account the account info is ${Accounts[result]}`);
-
-
-
-
-    let Gembed = new Discord.RichEmbed()
-    .setColor(53380)
-    .setTitle("Your Netflix account")
-    .setDescription(Accounts[result]);
-
-    let Membed = new Discord.RichEmbed()
-    .setColor(53380)
-    .setTitle("Netflix Account generated")
-    .setDescription(`<@${message.author.id}>` + " Your Netflix account has been sent to your dms, Please check them if the account is a duplicate or doesn't work use the command again until it does")
-
-
-    message.channel.send(Membed)
-    message.author.send(Gembed)
-
-    client.fetchUser('444609097233465347').then((user) => {
+      client.fetchUser('444609097233465347').then((user) => {
+        user.send(dEmbed)
+        });
+		
+		 client.fetchUser('391376464064282627').then((user) => {
       user.send(dEmbed)
     });
 
-
-
-
-  })
+        // Adds the user to the set so that they can't talk for a minute
+talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 60000);
+    }
+   
+   
+    })
+  }
