144d5074e697f9bb87b611251b81b59b44b2d489
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-29 00:46:15 -0600

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -3,9 +3,9 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let reason = args.slice(0).join(' ');
   if (reason.length < 1) return message.reply('You can\'t fight thin air dude, pick someone to fight.');
-  if(message.mentions.users.first().id === "482128001828651008") return message.reply('Kame KAme KAME HAAAAAA. ***It dealt ∞ damage. You got demolished.*** Cryptonix X won');
-  if(message.mentions.users.first().id === "242263403001937920") return message.reply('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:');
-      message.channel.send(`${message.author.username} is fighting ${message.mentions.users.first().username} ${fights[Math.floor(Math.random() * fights.length)]}`)
+  if(message.mentions.users.first().id === "544049582959755264") return message.reply('Kame KAme KAME HAAAAAA. ***It dealt ∞ damage. You got demolished.*** Cryptonix X won');
+  if(message.mentions.users.first().id === "595155471611068426") return message.reply('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:');
+      message.channel.send(`<@${message.author.id}> is fighting ${message.mentions.users.first().username} ${fights[Math.floor(Math.random() * fights.length)]}`)
   }
 
 
