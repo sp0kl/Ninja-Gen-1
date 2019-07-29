3027d03e3f470ce6316ef191a5a7aa2cd1cb6cc1
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-29 01:00:03 -0600

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
   if (reason.length < 1) return message.reply('You can\'t fight thin air dude, pick someone to fight.');
   if(message.mentions.users.first().id === "544049582959755264") return message.reply('Kame KAme KAME HAAAAAA. ***It dealt ∞ damage. You got demolished.*** Ninja Gen won');
   if(message.mentions.users.first().id === "595155471611068426") return message.reply('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:');
-  if (user.id === message.author.id) return.message.reply(`<@${message.author.id}> Self harm is bad :facepalm: go take a time out kid!`)
+  if (user.id === message.author.id) return message.reply(`<@${message.author.id}> Self harm is bad :facepalm: go take a time out kid!`)
       message.channel.send(`<@${message.author.id}> is fighting ${message.mentions.users.first().username} ${fights[Math.floor(Math.random() * fights.length)]}`)
   }
 
