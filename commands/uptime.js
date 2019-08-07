6c91613072a0e078d1f8392c4783bb16216cee22
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-07 01:36:23 -0600

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,6 +7,7 @@ exports.run = (client, message) => {
         hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24),
         days = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
 
+        days = (days < 10) ? "0" + days : days; 
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
