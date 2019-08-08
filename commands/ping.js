b8f19e8718e80c6e86d18991c29183e8ad908ab9
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-07 23:14:14 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,12 @@
 exports.run = async (client, message, args) => {
   let ping = client.ping;
-  message.channel.send("The bots ping is " + client.ping + "ms!");
+
+  let Pembed2 = new Discord.RichEmbed()
+      .setColor(0xff0000)
+      .setTitle("Ninja Gen Ping Check :thumbsup:")
+      .setDescription("The bots ping is " + client.ping + "ms!");
+      message.delete().catch();
+
+
+  message.channel.send(Pembed2);
 };
