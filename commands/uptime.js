4098f31705602e4b823dc119f0ac875281c55eff
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-07 22:10:04 -0600

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,23 +3,23 @@ const Discord = require('discord.js');
 exports.run = (client, message) => {
     var milliseconds = parseInt((client.uptime % 1000) / 100),
         seconds = parseInt((client.uptime / 1000) % 60),
-        minutes = parseInt((client.uptime / (1000 * 60)) % 60),
+        minutes = parseInt((client.uptime / (1000 * 60)) % 24),
         hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24),
         days = parseInt((client.uptime / (1000 * 60 * 60 * 60)) % 360);
 
         days = (days < 10) ? "0" + "0" + days : days; 
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        hours = (hours < 10) ? "0" + "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + "0" + seconds : seconds;
 
    let Uembed = new Discord.RichEmbed() 
       .setTitle(":clock1: Ninja Gen Uptime :clock1:")
       .setDescription("I have been online for")
       .setColor(53380)
       .addField("Days", "** " + days + " **")
-      .addField("Hours", "** " + hours + " **Hours")
-      .addField("Minutes", "**" + minutes + " **Minutes")
-      .addField("Seconds", "**" + seconds + "." + milliseconds + " **Seconds")
+      .addField("Hours", "** " + hours + " **")
+      .addField("Minutes", "**" + minutes + " **")
+      .addField("Seconds", "**" + seconds + "." + milliseconds + " **")
       .setFooter(`© Ninja Gen Beta`);
         message.delete().catch();
         message.channel.send(Uembed);
