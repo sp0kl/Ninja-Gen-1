0beff84ff8fc8d484ce3baf698ba78d3d7f8cfc6
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-07 21:27:02 -0600

    Update Categories Embed Color

--- commands/categories.js
+++ commands/categories.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   let hEmbed = new Discord.RichEmbed()
    .setTitle("Account Categories")
    .setDescription(`<@${message.author.id}>` + " Here you can see a list of accounts i offer and how many are currently in stock **Please note:** Some accounts may not work, you can always join our support server or DM us to let us know so we can remove them :thumbsup:")
-   .setColor("53380")
+   .setColor("0x0ffff")
    .addField("Minecraft", "**0** Accounts available")
    .addField("Spotify", "**19** Accounts available")
    .addField("Dominos", "**20** Accounts available")
