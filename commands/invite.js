5d85bc95ac28127f0fc59e4149412cc264f95f80
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 14:53:56 -0600

    Unlocked Invite Command

--- commands/invite.js
+++ commands/invite.js
@@ -4,20 +4,6 @@ const owners = require ("./config.json");
 
 exports.run = async (client, message, args) => {
   
-          let Uembed = new Discord.RichEmbed() 
-      .setTitle("Ninja Gen Invite")
-      .setDescription(`<@${message.author.id}>` + " Sorry i am not yet available to be invited to servers. This feature will be available after my Beta stages :frowning:")
-      .setColor("0xff0000")
-      .addField("Current Verison", "Beta v1.00")
-      .addField("Next Version", "Beta v1.01")
-      .addField("Expected Full Release Date", "August/14/2019")  
-    message.delete().catch();
-   
-   
-   
-    if(message.author.id !== owners) return message.channel.send(Uembed)
-        snekfetch.get(`http://ip-api.com/json/${args}`).then (r => {
-          message.delete().catch();
     
    let hEmbed = new Discord.RichEmbed()
    .setTitle("Ninja Gen Invite")
@@ -26,6 +12,7 @@ exports.run = async (client, message, args) => {
    .addField("You Can Invite Me Here", "[Ninja Gen Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=544049582959755264&permissions=2146958839&scope=bot)")
     .setFooter(`Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
    .setThumbnail(`https://i.imgur.com/xerUkNI.png`);
-   message.channel.send(hEmbed)    
+   message.channel.send(hEmbed)
+   message.delete().catch() 
     })
 }   
