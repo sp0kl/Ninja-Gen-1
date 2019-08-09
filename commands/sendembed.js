bc0cf6e679c30e4eda0720dde0622dc1609836c9
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 16:28:59 -0600

    Attempt To Add User Set Title

--- commands/sendembed.js
+++ commands/sendembed.js
@@ -7,12 +7,14 @@ exports.run = async (client, message, args) => {
 
 let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));    
     const Message = args.join(" ");
+    const Title = args.join(" ");
     let sicon = message.guild.iconURL;
     message.delete().catch();
 
-    if(!args[0] || args[0 == "help"]) return message.reply("Usage: nb/sendembed <Embed message>");
+    if(!args[0] || args[0 == "help"]) return message.reply("Usage: nb/sendembed <Embed Title> <Embed message>");
   let embedMessage = new Discord.RichEmbed()
   
+  .setTitle(Title)
   .setDescription(Message)
   .setThumbnail(sicon)
   .setColor("#0x3dfbff")
