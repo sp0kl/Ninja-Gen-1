e6462ad9a341fac29b36a776118fcacedb792ce1
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 17:15:59 -0600

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -3,8 +3,16 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let reason = args.slice(0).join(' ');
   message.delete().catch()
+
+let bot = client.users.get('544049582959755264');  
+   message.delete().catch();
+   let fEmbed1 = new Discord.RichEmbed()
+   .setTitle("Fight Club :punch:")
+   .setColor("0x0ffff")
+   .setDescription(message.reply('Kame KAme KAME HAAAAAA. ***It dealt ∞ damage. You got demolished.*** Ninja Gen won')
+
   if (reason.length < 1) return message.reply('You can\'t fight thin air dude, pick someone to fight.');
-  if(message.mentions.users.first().id === "544049582959755264") return message.reply('Kame KAme KAME HAAAAAA. ***It dealt ∞ damage. You got demolished.*** Ninja Gen won');
+  if(message.mentions.users.first().id === "544049582959755264") return message.channel.send(fEmbed1);
   if(message.mentions.users.first().id === "595155471611068426") return message.reply('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:');
   if (user.id === message.author.id) return message.reply(`Self harm is bad :facepalm: go take a time out kid!`)
       message.channel.send(`<@${message.author.id}> is fighting ${message.mentions.users.first().username} ${fights[Math.floor(Math.random() * fights.length)]}`)
