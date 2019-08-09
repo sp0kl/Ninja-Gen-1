c97ce270fc2f03cb65b3a9125417be2b4378afb1
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 17:33:55 -0600

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -21,7 +21,7 @@ exports.run = (client, message) => {
       .addField("Hours", "** " + hours + " **")
       .addField("Minutes", "**" + minutes + " **")
       .addField("Seconds", "**" + seconds + "." + milliseconds + " **")
-      .setFooter(`Last Restarted`);
+      .setFooter(message.author.Avatar, `Last Restarted`);
         message.delete().catch();
         message.channel.send(Uembed);
 }
