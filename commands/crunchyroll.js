3d9a8f5249cc7c3871942eb6aeb72ccc58ff822c
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 16:15:06 -0600

    CrunchyRoll - Add Stock Dates.

--- commands/crunchyroll.js
+++ commands/crunchyroll.js
@@ -36,6 +36,10 @@ exports.run = async (client, message, args) => {
     .setColor(53380)
     .setTitle("CrunchyRoll Account generated")
     .setDescription(`<@${message.author.id}>` + " Your CrunchyRoll account has been sent to your dms, Please check them if the account is a duplicate or doesn't work use the command again until it does")
+    .addField("Last Restock Date", "August/05/2019")
+    .addField("Next Restock Date", "August/30/2019")
+    .setFooter(`© Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
+
 
 
     message.channel.send(Membed)
