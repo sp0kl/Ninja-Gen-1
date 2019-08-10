bc6458f221cee2954871653ea906d206c7a4e3c0
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-10 04:26:20 -0600

    Categories - Update Spotify Count

--- commands/categories.js
+++ commands/categories.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
    .setDescription(`<@${message.author.id}>` + " Here you can see a list of accounts i offer and how many are currently in stock **Please note:** Some accounts may not work, you can always join our support server or DM us to let us know so we can remove them :thumbsup:")
    .setColor("0x0ffff")
    .addField("Minecraft", "**0** Accounts available")
-   .addField("Spotify", "**19** Accounts available")
+   .addField("Spotify", "**46** Accounts available")
    .addField("Dominos", "**20** Accounts available")
    .addField("Netflix", "**0** Accounts available")
    .addField("CrunchyRoll", "**19** Accounts available")
