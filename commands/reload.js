c73ccf7681580d4bcb6ce455310ea46977c29a06
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-10 03:57:59 -0600

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,4 +1,4 @@
-//These are the node modules required to run the bot and use the commands
+//These are the node modules required to run the bot and use the commands DO NOT remove these
 const Discord = require("discord.js");
 const Music = require('discord.js-musicbot-addon');
 const Enmap = require("enmap");
@@ -11,13 +11,46 @@ const config = require('../Data/config.json');
 const owners = require ('../Data/config.json');
 client.config = config;
 
+//These are our message embeds, to make the bots command responses look cleaner.
+ let rEmbed1 = new Discord.RichEmbed()
+   .setTitle("About Ninja Gen")
+   .setColor("0x0ffff")
+   .addField("Current Version", "**BETA** v1.00")
+   .addField("Owner/Author", `Created By: ${owner}`)
+   .addField("Coders/Developers", `${owner}`)
+   .addField("Ninja Gen Support", "[Support Server](https://discord.gg/CRhNHBs)")
+     .setFooter(`© Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
+    .setThumbnail(`https://i.imgur.com/xerUkNI.png`)
+   message.channel.send(aEmbed)
+ 
+ 
+ let rEmbed2 = new Discord.RichEmbed()
+   .setTitle("Whoops Something Went Wrong")
+   .setColor(0xff0000)
+   .setDescription(":thinking: Sorry but that command does not exist, If you need help please use ``gen help`` to see a list of commands :shrug:")
+     .setFooter(`© Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
+    .setThumbnail(`https://i.imgur.com/xerUkNI.png`)
+ 
+ 
+ let rEmbed3 = new Discord.RichEmbed()
+   .setTitle("About Ninja Gen")
+   .setColor("0x0ffff")
+   .addField("Current Version", "**BETA** v1.00")
+   .addField("Owner/Author", `Created By: ${owner}`)
+   .addField("Coders/Developers", `${owner}`)
+   .addField("Ninja Gen Support", "[Support Server](https://discord.gg/CRhNHBs)")
+     .setFooter(`© Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
+    .setThumbnail(`https://i.imgur.com/xerUkNI.png`)
+   message.channel.send(aEmbed)
+ 
 
+//This is our command code and args.
 exports.run = (client, message, args) => {
    if(message.author.id !== config.owners) return message.channel.send("You cannot use this command it is **BOTOWNER** only!")
   if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
   const commandName = args[0];
   if(!client.commands.has(commandName)) {
-    return message.reply("That command does not exist");
+    return message.channel.send(rEmbed2);
   }
   delete require.cache[require.resolve(`./${commandName}.js`)];
   client.commands.delete(commandName);
