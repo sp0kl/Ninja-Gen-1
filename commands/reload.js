76607958ba4bad669119d0808626336a3439002c
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-10 03:47:02 -0600

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,19 @@
+//These are the node modules required to run the bot and use the commands
+const Discord = require("discord.js");
+const Music = require('discord.js-musicbot-addon');
+const Enmap = require("enmap");
+const fs = require("fs");
+const snekfetch = require('snekfetch');
+const talkedRecently = new Set();
+const token = process.env.BOT_TOKEN;
+const client = new Discord.Client();
+const config = require('../Data/config.json');
+const owners = require ('../Data/config.json');
+client.config = config;
+
+
 exports.run = (client, message, args) => {
-   if(message.author.id !== 444609097233465347) return message.channel.send("You cannot use this command it is **BOTOWNER** only!")
-   if(message.author.id !== 444609097233465347) return message.channel.send("You cannot use this command it is **BOTOWNER** only!")
+   if(message.author.id !== config.owners) return message.channel.send("You cannot use this command it is **BOTOWNER** only!")
   if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
   const commandName = args[0];
   if(!client.commands.has(commandName)) {
