6cb4f3605f5730add17f29f72c282c32a168bfd6
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-09 21:57:05 -0600

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -5,21 +5,42 @@ exports.run = (client, message, args) => {
   let reason = args.slice(0).join(' ');
   message.delete().catch()
 
-let bot = client.users.get('544049582959755264');  
+let bot = client.users.get('544049582959755264');
+let nb = client.users.get('595155471611068426');  
    message.delete().catch();
    let fEmbed1 = new Discord.RichEmbed()
    .setTitle("Fight Club :punch:")
    .setColor("0x0ffff")
    .setDescription('WHAT!? Hiyah!!!.')
    .addField(`Fight Results`, `${bot} Karate Chopped You and dealy ∞ Damage ***YOU GOT DESTROYED***`)
-   .addField(`Winner`, `${bot}`)
+   .addField(`Winner`, ``)
    .addField(`Loser`, `<@${message.author.id}>`);
+  
+   message.delete().catch();
+   let fEmbed2 = new Discord.RichEmbed()
+   .setTitle("Fight Club :punch:")
+   .setColor("0x0ffff")
+   .setDescription('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:')
+   .addField(`Winner`, `${nb}`)
+   .addField(`Loser`, `<@${message.author.id}>`);
+   
+   message.delete().catch();
+  let fEmbed3 = new Discord.RichEmbed
+  .setTitle("Fight Club :punch:")
+  .setColor("0x0ffff")
+  .setDescription("Self harm is bad :facepalm: go take a time out kid!")
+  
+  message.delete().catch();
+  let fEmbed4 = new Discord.RichEmbed
+  .setTitle("Fight Club :punch:")
+  .setColor("0x0ffff")"
+  .setDescription(`<@${message.author.id}> is fighting <@${message.mentions.users.first().id}> ${fights[Math.floor(Math.random() * fights.length)]}`)
 
   if (reason.length < 1) return message.reply('You can\'t fight thin air dude, pick someone to fight.');
   if(message.mentions.users.first().id === "544049582959755264") return message.channel.send(fEmbed1);
-  if(message.mentions.users.first().id === "595155471611068426") return message.reply('You can\'t fight him you pleblord.:facepalm: He will destroy you:wink:');
-  if (user.id === message.author.id) return message.reply(`Self harm is bad :facepalm: go take a time out kid!`)
-      message.channel.send(`<@${message.author.id}> is fighting <@${message.mentions.users.first().id}> ${fights[Math.floor(Math.random() * fights.length)]}`)
+  if(message.mentions.users.first().id === "595155471611068426") return message.channel.send(fEmbed2);
+  if (user.id === message.author.id) return message.channel.send(fEmbed3)
+      message.channel.send(fEmbed4)
   }
 
 
