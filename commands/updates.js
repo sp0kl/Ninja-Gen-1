60687b391cfd0105dd54dd13795ad2af8f8a19c3
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-10 04:59:49 -0600

    Add files via upload

---
+++ commands/updates.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
+const fs = require("fs");
+const owners = require ('../Data/config.json');
+
+exports.run = async (client, message, args) => {
+    
+
+let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));   
+    const Message = args.join(" ");
+    let sicon = message.guild.iconURL;
+    message.delete().catch();
+
+    if(!args[0] || args[0 == "help"]) return message.reply("Usage: nb/sendembed <Embed Title> <Embed message>");
+  let embedMessage = new Discord.RichEmbed()
+  
+  .setTitle("Ninja Gen Updates")
+  .setDescription(Message)
+  .setThumbnail(sicon)
+  .setColor("#0x3dfbff")
+    
+  message.channel.send(embedMessage);
+  }
