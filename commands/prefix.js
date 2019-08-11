19e9195decb15e2e7234942e8ccb227615137111
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-11 01:04:20 -0600

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,23 +1,15 @@
 //Required Node Modules go here!
 const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
-const serverSettings = require ('../Data/config.json');
+const guildConf = require ('../Data/config.json');
+const config = require ('../Data/config.json');
 const bot = new Discord.Client(); // Our Discord Client defined as bot
 exports.run = async (client, message, args) => {
 
 	//Allows a user to set a prefix for the server.
-
-	Prefix = function setPrefix(message, command, args, serverSettings) {
-		args = args.filter(arg => arg.trim().length > 0);
-		if (args.length === 0) {
-			message.reply(`To set prefix please use ${command} <prefix>`).catch(console.error);
-			return;
-		}
-		const newPrefix = args[0];
-		serverSettings.prefix = newPrefix;
-		bot.setServerSettings(message.guild.id, serverSettings);
-		message.channel.send(`**Prefix has been set to:** \`${newPrefix}\``).catch(console.error);
-		return;
-	};
+guildConf[message.guild.id].prefix = args[0];
+	if (!guildConf[message.guild.id].prefix) {
+		guildConf[message.guild.id].prefix = config.prefix; // If you didn't specify a Prefix, set the Prefix to the Default Prefix
+	}
 	
 };
