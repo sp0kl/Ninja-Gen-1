0d6276043eaffd4f2ae1629705f9c313dc476d8e
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-10 20:14:19 -0600

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -4,15 +4,6 @@ const owners = require ('../Data/config.json');
 
 exports.run = async (client, message, args) => {
 
-client.on('message', message => {
-	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
-	if (!prefixRegex.test(message.content)) return;
-
-	const [, matchedPrefix] = message.content.match(prefixRegex);
-	const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-	const command = args.shift();
-
-	
 	  let owner = client.users.get('444609097233465347');
    let dev = client.users.get('391376464064282627'); 
    let bot = client.users.get('544049582959755264');  
