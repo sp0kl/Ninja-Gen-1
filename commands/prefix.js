985bf1876d0eab7e9f3fe8c5aadbb955020132a8
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-11 00:47:36 -0600

    Add files via upload

---
+++ commands/prefix.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+	//Allows a user to set a prefix for the server.
+
+	setPrefix = function setPrefix(message, command, args, serverSettings) {
+		args = args.filter(arg => arg.trim().length > 0);
+		if (args.length === 0) {
+			message.reply(`To set prefix please use ${command} <prefix>`).catch(console.error);
+			return;
+		}
+		const newPrefix = args[0];
+		serverSettings.prefix = newPrefix;
+		bot.setServerSettings(message.guild.id, serverSettings);
+		message.channel.send(`**Prefix has been set to:** \`${newPrefix}\``).catch(console.error);
+		return;
+	};
+	
+};
