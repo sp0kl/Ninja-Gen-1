efb7272805e67716882505da42112c420a1f30e9
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-11 19:27:42 -0600

    Update catergories.js

--- commands/catergories.js
+++ commands/catergories.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   
   let hEmbed = new Discord.RichEmbed()
-   .setTitle("Account Categories")
+   .setTitle("Account Catergories")
    .setDescription(`<@${message.author.id}>` + " Here you can see a list of accounts i offer and how many are currently in stock **Please note:** Some accounts may not work, you can always join our support server or DM us to let us know so we can remove them :thumbsup:")
    .setColor("0x0ffff")
    .addField("Minecraft", "***0*** Accounts available")
