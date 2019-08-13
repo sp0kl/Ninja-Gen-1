ad1020b559c5c03b90f9797d981cf4e931176fb2
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-13 00:40:02 -0600

    Update origin.js

--- commands/origin.js
+++ commands/origin.js
@@ -4,6 +4,17 @@ const talkedRecently = new Set();
 const owners = require ('../Data/config.json');
 
 exports.run = async (client, message, args) => {
+let bot = client.users.get('544049582959755264'); //Ninja Gen#1378 
+let Uembed = new Discord.RichEmbed()
+      .setColor("0xff0000")
+      .setTitle("Fortnite Command Lockdown")
+      .setDescription(`<@${message.author.id}>` + " Uh-Oh this command is on Lockdown, Find out why below")
+      .addField("Lockdown Reason", "Empty Account Category Fortnite accounts are ***Coming Soon***")
+      .addField("Lockdown Initiated By", `${bot} Owner/Developer`)
+      .addField("Lockdown Release Date", "**To Be Announced**, Join my [support server](https://discord.gg/JQqCfcU) for more info")
+      message.delete().catch();
+   
+     if (message.author.id !== '444609097233465347') return message.channel.send(Uembed);
 	
 	 let Cembed = new Discord.RichEmbed() 
       .setTitle("Hmm :thinking: To Fast!!")
@@ -14,19 +25,6 @@ exports.run = async (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
             message.channel.send(Cembed);
     } else {
-
-let Uembed = new Discord.RichEmbed()
-      .setColor("0xff0000")
-      .setTitle("Origin Accounts Status")
-      .setDescription(`<@${message.author.id}>` + " Sorry this category is currently unavailable :frowning: Join our [Support Server](https://discord.gg/CRhNHBs) for updates when this category becomes available");
-      message.delete().catch();
-   
-   
-   
-    if(message.author.id !== owners) return message.channel.send(Uembed)
-        snekfetch.get(`http://ip-api.com/json/${args}`).then (r => {
-          message.delete().catch();
-     
     let Accounts = ["COMING SOON"];
 
     let result = Math.floor((Math.random() * Accounts.length));
@@ -71,4 +69,3 @@ talkedRecently.add(message.author.id);
    
    
     }
-  }
