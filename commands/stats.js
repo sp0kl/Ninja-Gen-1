4ed49e89b413f4d1c28b95f6c5a2e73b92b2d503
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 23:55:07 -0600

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -15,7 +15,7 @@ const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [m
    .addField("Total Servers", `${client.guilds.size.toLocaleString()}`)
    .addField("Total Channels", `${client.channels.size.toLocaleString()}`)
    .addField("Current Discord.js Version", `v${version}`)
-   .addField("Current Node Version", `${process.version}`, {code: "asciidoc"})`);
+   .addField("Current Node Version", ``${process.version}`, {code: "asciidoc"})`)
    .setFooter("© Ninja Gen Beta", "https://i.imgur.com/xerUkNI.png")
    message.channel.send(hEmbed)
    message.delete().catch();	
