7d073f8e439289116818cdcc0ac52143f0dbf915
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-12 23:46:01 -0600

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,16 +3,23 @@ const moment = require("moment");
 require("moment-duration-format");
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  message.channel.send(`= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
-};
+const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+  let hEmbed = new Discord.RichEmbed()
+   .setTitle("Ninja Gen Staistics")
+   .setColor("0x0ffff")
+   .setDescription(`<@${message.author.id}>` + " My node statistics and VPS stats can be found below")
+   .addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
+   .addField("Current Uptime", `${duration}`);
+  .addField("Total Users", `${client.users.size.toLocaleString()}`);
+   .addField("Total Servers", `${client.guilds.size.toLocaleString()}`);
+   .addField("Total Channels", `${client.channels.size.toLocaleString()}`);
+   .addField("Current Discord.js Version", `v${version}`);
+   .addField("Current Node Version", `${process.version}`, {code: "asciidoc"});`);
+   .setFooter(`© Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`);
+   .setThumbnail(`https://i.imgur.com/xerUkNI.png`);
+   message.channel.send(hEmbed)
+   message.delete().catch();	
+ };
 
 exports.conf = {
   enabled: true,
