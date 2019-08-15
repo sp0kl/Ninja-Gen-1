820f549aeaf99717cf8958f6bd93c9f63976c020
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-14 19:36:28 -0600

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -10,8 +10,8 @@ exports.run = async (client, message, args) => {
    .setDescription(`<@${message.author.id}>` + " Interested in inviting me to your server? Use the link below")
    .setColor("0x0ffff")
    .addField("You Can Invite Me Here", "[Ninja Gen Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=544049582959755264&permissions=2146958839&scope=bot)")
-    .setFooter(`Ninja Gen Beta`, `https://i.imgur.com/xerUkNI.png`)
-   .setThumbnail(`https://i.imgur.com/xerUkNI.png`);
+    .setFooter(`Ninja Gen Beta`, `https://cdn.discordapp.com/avatars/544049582959755264/b5f7f7b81321f647ff62c991871bae43.png?size=2048?size=1024`)
+   .setThumbnail(`https://cdn.discordapp.com/avatars/544049582959755264/b5f7f7b81321f647ff62c991871bae43.png?size=2048?size=1024`);
    message.channel.send(hEmbed)
    message.delete().catch() 
 }   
